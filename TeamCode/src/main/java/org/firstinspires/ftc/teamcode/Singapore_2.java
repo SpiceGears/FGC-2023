16f53eb22c7ffc02a027a16aaae8f60aa2f04180
Bartłomiej Dylewski <dylewskipoczta@gmail.com>
2023-09-29 15:21:21 +0200

    update bucket safety for servo
    create functions for setters and setups

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Singapore_2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Singapore_2.java
@@ -7,49 +7,40 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+import java.time.Clock;
+import java.util.Timer;
+
 @TeleOp(name="Singapore_2", group="Linear Opmode")
 // @Disabled
 public class Singapore_2 extends LinearOpMode {
 
-    // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
 
-    //declare hardware
-    private DcMotor leftFrontDrive = null;
-    private DcMotor leftRearDrive = null;
-    private DcMotor rightFrontDrive = null;
-    private DcMotor rightRearDrive = null;
-    private DcMotor intakeMotor1 = null;
-    private DcMotor intakeMotor2 = null;
-    private DcMotor elevatorMotor1 = null;
-    private DcMotor elevatorMotor2 = null;
-    private Servo bucketServo1 = null;
-    private Servo bucketServo2 = null;
 
-    private double averageRobotSpeed = 0;
-    private double speedNow = 0;
-    private double speedSum = 0;
-    private double speedCount = 0;
+    // declare hardware
+    private DcMotor leftFrontDrive, leftRearDrive = null;
+    private DcMotor rightFrontDrive, rightRearDrive = null;
+    private DcMotor intakeMotor1, intakeMotor2 = null;
+    private DcMotor elevatorMotor1, elevatorMotor2 = null;
+    private Servo bucketServo1, bucketServo2 = null;
 
-    double elevatorMoveSpeed = 1;
-    int startElevatorPosition = 0;
-    int maxElevatorPosition = 4700;
-    int minElevatorPosition = 60;
-    int midElevatorPosition = 3400;
+    // elevator
+    double elevatorSpeed = 1;
+    final int startElevatorPosition = 0;
+    final int minElevatorPosition = 60;
+    final int midElevatorPosition = 3400;
+    final int maxElevatorPosition = 4700;
 
-    double bucketFlipSpeed = 0.6;
-    int bucketRestPosition = 0;
-    int bucketFlipPosition = 100;
-    int bucketFlipTreshold = 3000; // if over this amount of ticks, can flip bucket
+    // insert angleToServo (angle from vertical (negative -> to front, positive -> to back) )
+    final double bucketRestPosition = angleToServo(-90); // 0.1(6)
+    final double bucketFlippedPosition = angleToServo(25); // 0.59
+    final int bucketFlipThreshold = 3000; // if over this amount of ticks, can flip bucket
 
-    // insert angleToServo(angle from vertical)
-    double servoStartPosition = angleToServo(-90);
-    double servoFlippedPosition = angleToServo(25);
-
-    // Setup a variable for each drive wheel to save power level for telemetry
-    double leftPower;
-    double rightPower;
+    // setup a variable for setting power and telemetry
+    double leftPower, rightPower;
     double intakePower;
+    private double averageRobotSpeed = 0;
+    private double speedNow, speedSum, speedCount = 0;
 
 
     @Override
@@ -60,8 +51,115 @@ public class Singapore_2 extends LinearOpMode {
         // map hardware to port names
         mapHardware();
 
+        // setup hardware properties
+        setupHardware();
 
-        // set hardware properties
+        // wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run once after pressing start
+        // setups motors and positions when match starts
+        setupOnMatchStart();
+
+        // runs until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // controls drivetrain
+            double drive =  gamepad1.left_stick_y;
+            double turn  = -gamepad1.right_stick_x;
+            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+            // controls intake
+            if(gamepad1.right_trigger > gamepad1.left_trigger) {
+                intakePower = gamepad1.right_trigger;
+                if(intakePower > 0.8) { intakePower = 1; } // make intake spin full throttle at 80% input
+
+            } else if(gamepad1.right_trigger < gamepad1.left_trigger) {
+                intakePower = -gamepad1.left_trigger;
+                if(intakePower < -0.8) { intakePower = -1; } // make intake spin full throttle at 80% input
+
+            } else { intakePower = 0; }
+
+
+
+            // controls bucket
+            if(gamepad1.b) {
+                setBucketPosition(bucketFlippedPosition);
+            } else {
+                setBucketPosition(bucketRestPosition);
+            }
+            // bucket safety feature
+            if(elevatorMotor1.getCurrentPosition() < bucketFlipThreshold) { // if elevator below bucket flip height threshold
+                if(bucketServo1.getPosition() >= 1.1 * bucketRestPosition) { // if bucket position isn't in acceptable position
+                    // stop elevator unless bucket is good to go
+                    setBucketPosition(bucketRestPosition);
+                    setElevatorSpeed(0);
+                }
+            } else { // if elevator over bucket flip height threshold and bucket is flipped
+                // go like nothing happened
+                setElevatorSpeed(elevatorSpeed);
+            }
+
+            // controls elevator
+            if(gamepad1.a){
+                setElevatorPosition(minElevatorPosition);
+            }
+            if (gamepad1.x) {
+                setElevatorPosition(midElevatorPosition);
+            }
+            if (gamepad1.y) {
+                setElevatorPosition(maxElevatorPosition);
+            }
+
+            // calculate for telemetry
+            speedNow = Math.abs(leftPower) + Math.abs(rightPower) / 2;
+            speedCount++;
+            speedSum += speedNow;
+            averageRobotSpeed = speedSum / speedCount;
+
+            // sends calculated power to wheels
+            sendPower();
+
+            // sends info about match and robot to driver station
+            sendTelemetry();
+
+        }
+
+    }
+
+
+    void setBucketPosition(double position) {
+        bucketServo1.setPosition(position);
+        bucketServo2.setPosition(position);
+    }
+
+    void setElevatorPosition(int position) {
+        elevatorMotor1.setTargetPosition(position);
+        elevatorMotor2.setTargetPosition(position);
+    }
+    void setElevatorSpeed(double power) {
+        elevatorMotor1.setPower(power);
+        elevatorMotor2.setPower(power);
+    }
+
+    // maps hardware to ports
+    void mapHardware() {
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "lf_d");
+        leftRearDrive = hardwareMap.get(DcMotor.class, "lr_d");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rf_d");
+        rightRearDrive = hardwareMap.get(DcMotor.class, "rr_d");
+        elevatorMotor1 = hardwareMap.get(DcMotor.class, "el");
+        elevatorMotor2 = hardwareMap.get(DcMotor.class, "el1");
+        intakeMotor1 = hardwareMap.get(DcMotor.class, "in");
+        intakeMotor2 = hardwareMap.get(DcMotor.class, "in1");
+        bucketServo1 = hardwareMap.get(Servo.class, "b_s1");
+        bucketServo2 = hardwareMap.get(Servo.class, "b_s2");
+    }
+
+    // setups robot hardware
+    void setupHardware() {
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         leftRearDrive.setDirection(DcMotor.Direction.REVERSE);
@@ -80,113 +178,49 @@ public class Singapore_2 extends LinearOpMode {
         leftRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-
         elevatorMotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         elevatorMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
 
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-        runtime.reset();
-
-        // run once after pressing start
+    // setups motors and positions when match starts
+    void setupOnMatchStart() {
         if(opModeIsActive()) {
-            elevatorMotor1.setTargetPosition(startElevatorPosition);
-            elevatorMotor2.setTargetPosition(startElevatorPosition);
+            setElevatorPosition(startElevatorPosition);
             elevatorMotor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             elevatorMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            elevatorMotor1.setPower(elevatorMoveSpeed);
-            elevatorMotor2.setPower(elevatorMoveSpeed);
-            bucketServo1.setPosition(servoStartPosition);
-            bucketServo2.setPosition(servoStartPosition);
+            setElevatorSpeed(elevatorSpeed);
+            setBucketPosition(bucketRestPosition);
         }
-
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-
-            double drive = gamepad1.left_stick_y;
-            double turn  =  -gamepad1.right_stick_x;
-
-            if(gamepad1.right_trigger > gamepad1.left_trigger) {
-                intakePower = gamepad1.right_trigger;
-            } else if(gamepad1.right_trigger < gamepad1.left_trigger) {
-                intakePower = -gamepad1.left_trigger;
-            } else {
-                intakePower = 0;
-            }
-
-            if(gamepad1.b) {
-                bucketServo1.setPosition(servoFlippedPosition);
-                bucketServo2.setPosition(servoFlippedPosition);
-            } else {
-                bucketServo1.setPosition(servoStartPosition);
-                bucketServo2.setPosition(servoStartPosition);
-            }
-
-
-            if(gamepad1.a){
-                elevatorMotor1.setTargetPosition(minElevatorPosition);
-                elevatorMotor2.setTargetPosition(minElevatorPosition);
-            }
-            if (gamepad1.x) {
-                elevatorMotor1.setTargetPosition(midElevatorPosition);
-                elevatorMotor2.setTargetPosition(midElevatorPosition);
-            }
-            if (gamepad1.y) {
-                elevatorMotor1.setTargetPosition(maxElevatorPosition);
-                elevatorMotor2.setTargetPosition(maxElevatorPosition);
-            }
-
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-
-            speedNow = Math.abs(leftPower) + Math.abs(rightPower) / 2;
-            speedCount++;
-            speedSum += speedNow;
-            averageRobotSpeed = speedSum / speedCount;
-
-
-            // Send calculated power to wheels
-            leftFrontDrive.setPower(leftPower);
-            rightFrontDrive.setPower(rightPower);
-            leftRearDrive.setPower(leftPower);
-            rightRearDrive.setPower(rightPower);
-            intakeMotor1.setPower(intakePower);
-            intakeMotor2.setPower(intakePower);
-
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-            telemetry.addData("Motors", "intake (%.2f)", intakePower);
-            telemetry.addData("avg robot speed", averageRobotSpeed);
-            telemetry.addData("Elevator1EncoderValue", elevatorMotor1.getCurrentPosition());
-            telemetry.addData("Elevator2EncoderValue", elevatorMotor2.getCurrentPosition());
-            telemetry.addData("Elevator1TargetPosition", elevatorMotor1.getTargetPosition());
-            telemetry.addData("Elevator2TargetPosition", elevatorMotor2.getTargetPosition());
-            telemetry.addData("ElevatorMode", elevatorMotor1.getMode());
-
-            telemetry.update();
-
-
-        }
-
     }
 
-    //map hardware to ports
-    void mapHardware() {
-        leftFrontDrive = hardwareMap.get(DcMotor.class, "lf_d");
-        leftRearDrive = hardwareMap.get(DcMotor.class, "lr_d");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "rf_d");
-        rightRearDrive = hardwareMap.get(DcMotor.class, "rr_d");
-        elevatorMotor1 = hardwareMap.get(DcMotor.class, "el");
-        elevatorMotor2 = hardwareMap.get(DcMotor.class, "el1");
-        intakeMotor1 = hardwareMap.get(DcMotor.class, "in");
-        intakeMotor2 = hardwareMap.get(DcMotor.class, "in1");
-        bucketServo1 = hardwareMap.get(Servo.class, "b_s1");
-        bucketServo2 = hardwareMap.get(Servo.class, "b_s2");
+    // sends info about robot to driver station
+    void sendTelemetry() {
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        telemetry.addData("Motors", "intake (%.2f)", intakePower);
+        telemetry.addData("avg robot speed", averageRobotSpeed);
+        telemetry.addData("Elevator1EncoderValue", elevatorMotor1.getCurrentPosition());
+        telemetry.addData("Elevator2EncoderValue", elevatorMotor2.getCurrentPosition());
+        telemetry.addData("Elevator1TargetPosition", elevatorMotor1.getTargetPosition());
+        telemetry.addData("Elevator2TargetPosition", elevatorMotor2.getTargetPosition());
+        telemetry.addData("ElevatorMode", elevatorMotor1.getMode());
+        telemetry.update();
     }
 
-    // calculates angle from vertical (positive -> front, negative -> back)
+    // sends calculated power to motors/servos
+    void sendPower() {
+        leftFrontDrive.setPower(leftPower);
+        rightFrontDrive.setPower(rightPower);
+        leftRearDrive.setPower(leftPower);
+        rightRearDrive.setPower(rightPower);
+        intakeMotor1.setPower(intakePower);
+        intakeMotor2.setPower(intakePower);
+    }
+
+    // calculates angle from vertical (negative -> front, positive -> back)
     double angleToServo(double angle) {
-        return angle = ((135 + angle )/ 270);
+        return ((135 + angle )/ 270);
+        // returned 0 -> 135degrees back from vertical
+        // returned 1 -> 135degrees front from vertical
     }
 }
